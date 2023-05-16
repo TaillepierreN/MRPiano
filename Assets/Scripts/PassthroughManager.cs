e7e3b39b3836e8025de1234b0ea4c09f2b1a1a22
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-05-16 14:51:22 +0200

    passthrough stylize

---
+++ Assets/Scripts/PassthroughManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassthroughManager : MonoBehaviour
+{
+    public OVRPassthroughLayer passthrough;
+    public OVRInput.Button button;
+    public OVRInput.Controller controller;
+    public List<Gradient> colorMapGradient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(button,controller))
+        {
+            passthrough.hidden = !passthrough.hidden;
+        }
+    }
+    public void SetOpacity(float value)
+    {
+        passthrough.textureOpacity = value;
+    }
+    public void SetColorMapGradient(int index)
+    {
+        passthrough.colorMapEditorGradient = colorMapGradient[index];
+    }
+        public void SetBrightness(float value)
+    {
+        passthrough.colorMapEditorBrightness = value;
+    }
+        public void SetContrast(float value)
+    {
+        passthrough.colorMapEditorContrast = value;
+    }
+        public void SetPosterize(float value)
+    {
+        passthrough.colorMapEditorPosterize = value;
+    }
+}
